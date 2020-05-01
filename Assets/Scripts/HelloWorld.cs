270f6ab60e48bad45cfc202c12d6a69915ba6c10
Jeff Anderson <captainqwyx@gmail.com>
2020-05-01 14:01:06 -0500

    remove frame counter

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -9,12 +9,12 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello Wolrd!");
+        Debug.Log("Hello Wolrd! This is TimeMaker!!! :)");
     }
 
     // Update is called once per frame
     void Update()
     {
-        print("new frame: " + frameNum++);
+
     }
 }
